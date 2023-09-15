858e8c11fc73de1ca375e9df36378a643b1e49e0
星野祐樹 <yhoshino@obi.sg>
2023-09-15 17:15:41 +0900

    js調整

--- src/access/js/_function.js
+++ src/access/js/_function.js
@@ -1,11 +1,38 @@
 import smoothscroll from 'smoothscroll-polyfill';
+import { _breakP, _ua, _log } from '../../common/js/lib/utils.js';
 export default class access {
   constructor() {
+    this.accessAccordion();
     this.normalAccordion();
     this.iosCheck();
   }
 
   // accessFunc(ACCESSページの関数)
+
+  accessAccordion() {
+    smoothscroll.polyfill();
+    window.addEventListener('load', function() {
+      const elms = document.querySelectorAll('.js-access-accr');
+      elms.forEach(elm => {
+        const cardTotalHeight = elm.nextElementSibling.clientHeight;
+        const cardInitHeight = 0;
+        elm.nextElementSibling.style.maxHeight = cardInitHeight + 'px';
+
+        // init
+        elm.nextElementSibling.style.maxHeight = '0px';
+
+        elm.addEventListener('click', () => {
+          elm.classList.toggle('is-open');
+          elm.nextElementSibling.classList.toggle('is-open');
+          if (elm.nextElementSibling.classList.contains('is-open')) {
+            elm.nextElementSibling.style.maxHeight = cardTotalHeight + 'px';
+          } else {
+            elm.nextElementSibling.style.maxHeight = cardInitHeight + 'px';
+          }
+        });
+      });
+    });
+  }
 
   normalAccordion() {
     smoothscroll.polyfill();
